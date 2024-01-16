214ffef1d1e092c04a89de1ec97b931356cae8ff
Andrew Kelton <109199143+AndrewKelton@users.noreply.github.com>
2024-01-15 21:40:50 -0500

    Add files via upload

---
+++ binarytodec.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#define LIMIT 50
+
+int main()
+{
+    int bin;
+
+    printf("Enter a binary number to be converted to decimal: ");
+    scanf("%d", &bin);
+
+    char binS[LIMIT];
+    int num = 0;
+    int add = 0;
+    int total = 0;
+
+    sprintf(binS,"%d", bin);
+    int x = 0;
+
+   printf("%s\n", binS);
+
+    for (int i = strlen(binS); i > 0; i--)
+    {
+        num = binS[x] - '0';
+        add = num * (pow(2, i-1));
+        total += add;
+        x++;
+    }
+
+    printf("%d\n", total);
+}
